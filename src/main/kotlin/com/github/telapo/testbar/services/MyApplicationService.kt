e6e43a9f716e0a14978e24f5df5c311467c81c8a
GitHub Action <action@github.com>
2020-09-02 09:30:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/telapo/testbar/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/telapo/testbar/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.telapo.testbar.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.telapo.testbar.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/telapo/testbar/services/MyProjectService.kt
+++ src/main/kotlin/com/github/telapo/testbar/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.telapo.testbar.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.telapo.testbar.MyBundle
 
 class MyProjectService(project: Project) {
 
